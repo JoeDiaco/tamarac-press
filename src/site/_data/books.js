2524d97866ba10b4d333dfde8e5ec3eb42184822
josephdiaco <94772870+JoeDiaco@users.noreply.github.com>
2022-01-12 16:06:16 -0500

    many changes

---
+++ src/site/_data/books.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "books" : [
+        {
+            "title" : "",
+            "author" : "",
+            "ISBN" : "",
+            "contentBlurb" : "",
+            "coverUrl" : "",
+            "amazonLink" : "",
+            "indieBoundLink" : ""
+        }
+    ]
+  }
+
+//   {
+//     "title" : "",
+//     "author" : "",
+//     "ISBN" : "",
+//     "contentBlurb" : "",
+//     "coverUrl" : "",
+//     "amazonLink" : "",
+//     "indieBoundLink" : ""
+// }
