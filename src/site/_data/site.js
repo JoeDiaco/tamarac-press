7d96062303f1cea56977bfb04fb81ba439564cef
josephdiaco <94772870+JoeDiaco@users.noreply.github.com>
2022-01-18 09:13:34 -0500

    started over

--- src/site/_data/site.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "rootUrl" : "http://thetamaracpress.com",
-  "title" : "The Tamarac Press",
-  "buildTime" : new Date(),
-  "author" : "Pat Goudey O'Brien",
-  "dev" : "Joe Diaco",
-  "description" : "Self publishing services company based in Vermont."
-}
