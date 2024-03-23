ac3cb3cfa6c66a9995f6b9f7f0bc399e5a3f49f4
JoeDiaco <joe@joediaco.com>
2024-03-22 21:56:05 -0400

    stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    
     return {
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
